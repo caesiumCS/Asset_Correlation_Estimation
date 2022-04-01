f412ab5ccfd804db1f31af00bd6d2be0a0f0ad6a
caesiumCS <caesiummm@yandex.ru>
2022-04-01 19:29:29 +0300

    Initial commit

---
+++ Config.py
@@ -0,0 +1,7 @@
+class Config:
+
+    START_DATA = '1970-01-01'
+    FINAL_DATA = '2015-01-01'
+    MINIMUM_DAYS_IN_DATA = 255 * 10 # 255 trading days in year
+    HEAD_DATA_PATH = '../Data/head_data.csv'
+    SandP500_PATH = '../Data/S&P500_companies.csv'
---
+++ DataDownloader.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from Config import Config
+import yfinance as yf
+from tqdm import tqdm
+
+class HeadDataDownloader:
+
+    def __init__(self):
+        self.config = Config()
+
+    def create_head_dataset(self):
+        root_data = pd.read_csv(self.config.SandP500_PATH)
+        left_time = pd.to_datetime(self.config.START_DATA.replace('-', ''), format='%Y%m%d')
+        right_time = pd.to_datetime(self.config.FINAL_DATA.replace('-', ''), format='%Y%m%d')   
+        tickers_to_save = []
+        for ticker in tqdm(np.unique(root_data['Symbol'])):
+            x = yf.Ticker(ticker)
+            try:
+                x = x.history(period='max')
+            except JSONDecodeError:
+                continue
+            x = x.loc[left_time:right_time]
+            total_days = int(str(right_time - left_time).split()[0])
+            if x.shape[0] <= total_days and x.shape[0]>=self.config.MINIMUM_DAYS_IN_DATA:
+                tickers_to_save.append(ticker)
+            
+            def f(el):
+                return el in tickers_to_save
+            
+            new_data = root_data[ root_data['Symbol'].apply(f) ]
+            new_data.to_csv(self.config.HEAD_DATA_PATH, index=False)
