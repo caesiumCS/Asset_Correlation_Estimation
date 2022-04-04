b6f41876ecc30cabd31ae8bc03014a4bd75add54
caesiumCS <caesiummm@yandex.ru>
2022-04-04 12:23:50 +0300

    input and output configs

--- Config.py
+++ Config.py
@@ -8,3 +8,13 @@ class Config:
     MINIMUM_DAYS_IN_DATA = 255 * 10 # 255 trading days in year
     HEAD_DATA_PATH = 'Data/head_data.csv'
     SandP500_PATH = 'Data/S&P500_companies.csv'
+
+    '''
+    Config for train and test dataset
+    '''
+    TRAIN_SIZE = 0.8
+    TEST_SIZE = 1 - TRAIN_SIZE
+    INPUT_TIME_SERIES_SIZE = 90 # Traiding days 
+    OUTPUT_TIME_SERIES_SIZE = 30 # Traiding days
+    TRAIN_BATCH_SIZE = 32
+    TEST_BATCH_SIZE = 32
--- TrainingDataset.py
+++ TrainingDataset.py
@@ -9,14 +9,14 @@ from TimeSeriesDataset import TimeSeriesDataset
 
 class TrainingDataset(Dataset):
 
-    def __init__(self, dataframe, train_size, test_size , x_window_size, y_window_size, train_batch_size, test_batch_size):
+    def __init__(self, dataframe , train_batch_size, test_batch_size):
         self.dataframe = dataframe
-        self.train_size = train_size
-        self.test_size = test_size
-        self.x_window_size = x_window_size
-        self.y_window_size = y_window_size
-        self.train_batch_size = train_batch_size
-        self.test_batch_size = test_batch_size
+        self.train_size = Config.TRAIN_SIZE
+        self.test_size = Config.TEST_SIZE
+        self.x_window_size = Config.INPUT_TIME_SERIES_SIZE
+        self.y_window_size = Config.OUTPUT_TIME_SERIES_SIZE
+        self.train_batch_size = Config.TRAIN_BATCH_SIZE
+        self.test_batch_size = Config.TEST_BATCH_SIZE
 
         self.left_time_border = pd.to_datetime(Config.START_DATA.replace('-',''), format = '%Y%m%d')
         self.right_time_border = pd.to_datetime(Config.FINAL_DATA.replace('-',''), format = '%Y%m%d')
@@ -25,19 +25,35 @@ class TrainingDataset(Dataset):
         for ind in range(dataframe.shape[0]):
             sector = dataframe.iloc[ind]['GICS Sector']
             ticker = dataframe.iloc[ind]['Symbol']
+
             if self.sectors_to_datasets.get(sector) is None:
                 self.sectors_to_datasets[sector] = dict()
+            
             data = np.array(yf.Ticker(ticker).history(period='max')[self.left_time_border:self.right_time_border]['Close'])
-            self.sectors_to_datasets[sector][ticker] = TimeSeriesDataset(data, ticker, sector, x_window_size, y_window_size)
+            data = np.diff(data) / data[:,1:]
+            separate_ind = int(len(data)*self.train_size)
+            data_train = data[:separate_ind]
+            data_test = data[separate_ind:]
+            self.sectors_to_datasets[sector][ticker]['Train'] = TimeSeriesDataset(data_train, ticker, sector, self.x_window_size, self.y_window_size)
+            self.sectors_to_datasets[sector][ticker]['Test'] = TimeSeriesDataset(data_test, ticker, sector, self.x_window_size, self.y_window_size)
 
     def get_train_len(self):
-        pass
+        return self.train_size
 
     def get_test_len(self):
-        pass
+        return self.test_size
 
     def get_train_batch(self):
         pass
 
     def get_test_batch(self):
+        pass
+
+    def prepare_label(self, label_1, label_2):
+        pass
+
+    def prepare_object(self, input_1, input_2):
+        pass
+
+    def print_info(self):
         pass
